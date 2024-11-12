13f8cc775aa7918bbbe0c01039b57c8ded622818
MihaVar <mykhailvarych@gmail.com>
2024-11-12 20:20:23 +0200

    feat: remade functions into one file

--- main.ts
+++ main.ts
@@ -1,5 +1,86 @@
-// Тестування функцій
+// Типи товарів
 
+type BaseProduct = { // Продукт
+    id: number;
+    name: string;
+    price: number;
+    description?: string; // Опис для продуктів
+};
+  
+type Electronics = BaseProduct & { // Тип електроніка
+    category: 'electronics';
+    brand: string;
+    warranty: number; // Гарантія в місяцях
+};
+  
+type Clothing = BaseProduct & { // Тип одяг
+    category: 'clothing';
+    size: string;
+    material: string;
+};
+  
+type Book = BaseProduct & { // Тип книжка
+    category: 'book';
+    author: string;
+    genre: string;
+    pages: number;
+};
+
+// Пошук продуктів
+const findProduct = <T extends BaseProduct>(products: T[], id: number): T | undefined => { // Функція для пошуку продукта
+    if (!Array.isArray(products) || typeof id !== 'number') { // Перевірка на коректність даніх
+      console.error("Некоректні вхідні дані для пошуку товару.");
+      return undefined;
+    }
+    return products.find(product => product.id === id);
+  };
+
+const filterByPrice = <T extends BaseProduct>(products: T[], maxPrice: number): T[] => { // Функція для сортування продуктів за ціною
+    if (!Array.isArray(products) || typeof maxPrice !== 'number') { // Перевірка на коректність даніх
+      console.error("Некоректні вхідні дані для фільтрації за ціною.");
+      return [];
+    }
+    return products.filter(product => product.price <= maxPrice);
+};
+
+// Кошик з продуктами
+type CartItem<T> = { // Товар в кошику
+    product: T;
+    quantity: number;
+};
+
+// Функція для додавання предметів у кошик
+const addToCart = <T extends BaseProduct>( 
+    cart: CartItem<T>[],
+    product: T,
+    quantity: number
+    ): CartItem<T>[] => { 
+    if (!Array.isArray(cart) || typeof quantity !== 'number' || quantity <= 0) { // Перевірка на коректність даних
+      console.error("Некоректні вхідні дані для додавання товару в кошик.");
+      return cart;
+    }
+
+    const existingItem = cart.find(item => item.product.id === product.id); // Пошук товару
+    
+    if (existingItem) { // Оновлення кількості якщо товар вже є в кошику
+        existingItem.quantity += quantity;
+    } 
+    else { // Додається новий товар в кошик
+      cart.push({ product, quantity });
+    }
+    
+    return cart;
+};
+
+const calculateTotal = <T extends BaseProduct>(cart: CartItem<T>[]): number => { // Функція для рахування загальної вартості предметів у кошику
+    if (!Array.isArray(cart)) { // Перевірка на коректність даних
+      console.error("Некоректні вхідні дані для розрахунку загальної вартості.");
+      return 0;
+    }
+    return cart.reduce((total, item) => total + item.product.price * item.quantity, 0); // Підрахунок суми товарів у кошику
+};
+
+// Тестування функцій
 const electronics: Electronics[] = [
     {
       id: 1,
