9245a6b00eed9e5362c914315b414efbefbbadd4
MihaVar <mykhailvarych@gmail.com>
2024-12-02 20:40:32 +0200

    feat: add enums

---
+++ enum/Enums.ts
@@ -0,0 +1,36 @@
+// Статус студента
+export enum StudentStatus {
+    Active = "Active",
+    Academic_Leave = "Academic_Leave",
+    Graduated = "Graduated",
+    Expelled = "Expelled",
+}
+
+// Типи курсів
+export enum CourseType {
+    Mandatory = "Mandatory",
+    Optional = "Optional",
+    Special = "Special",
+}
+
+// Семестри
+export enum Semester {
+    First = "First",
+    Second = "Second",
+}
+
+// Оцінки
+export enum Grade {
+    Excellent = 5,
+    Good = 4,
+    Satisfactory = 3,
+    Unsatisfactory = 2,
+}
+
+// Факультети
+export enum Faculty {
+    Computer_Science = "Computer_Science",
+    Economics = "Economics",
+    Law = "Law",
+    Engineering = "Engineering",
+}
